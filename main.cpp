b467005616417c2b79028d433d909904240aa56c
p10tr13 <160889621+p10tr13@users.noreply.github.com>
2025-01-19 18:11:27 +0100

    Matrial and light properties

--- main.cpp
+++ main.cpp
@@ -192,6 +192,15 @@ int main()
 		lightingShader.setMat4("view", view);
 		lightingShader.setVec3("lightPos", lightPos);
 
+		lightingShader.setVec3("material.ambient", glm::vec3(1.0f, 0.5f, 0.31f));
+		lightingShader.setVec3("material.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
+		lightingShader.setVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
+		lightingShader.setFloat("material.shininess", 32.0f);
+
+		lightingShader.setVec3("light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
+		lightingShader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+		lightingShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
 		glm::mat4 model = glm::mat4(1.0f);
 		lightingShader.setMat4("model", model);
 
--- shader.fs
+++ shader.fs
@@ -2,6 +2,21 @@
 
 out vec4 FragColor;
 
+struct Material
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;    
+    float shininess;
+}; 
+
+struct Light
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 in vec2 TextCoord;
 in vec3 Normal;
 in vec3 FragPos;
@@ -10,20 +25,21 @@ in vec3 LightPos;
 uniform sampler2D texture1;
 uniform vec3 lightColor;
 
+uniform Light light;
+uniform Material material;
+
 void main()
 {
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(LightPos - FragPos);
     vec3 diffuse = max(dot(norm, lightDir), 0.0) * lightColor;
 
-    float ambientStrenght = 0.1;
-    vec3 ambient = ambientStrenght * lightColor;
+    vec3 ambient = light.ambient * material.ambient;
 
-    float specularStrength = 0.5;
     vec3 viewPos = vec3(0.0f, 0.0f, 0.0f);
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm); 
-    vec3 specular = pow(max(dot(viewDir, reflectDir), 0.0), 128) * specularStrength * lightColor;
+    vec3 specular = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess) * material.specular * light.specular * lightColor;
 
     FragColor = texture(texture1, TextCoord) * vec4(ambient + diffuse + specular, 1.0);
 };
